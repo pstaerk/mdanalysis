d5e031011a90ce42cd87f773a434110ed585bad5
Philipp Stärk <philipp.staerk@t-online.de>
2022-04-06 14:24:34 +0200

    Able to parse arbitrary columns now.

--- package/MDAnalysis/coordinates/LAMMPS.py
+++ package/MDAnalysis/coordinates/LAMMPS.py
@@ -611,9 +611,15 @@ class DumpReader(base.ReaderBase):
 
         # Create the data arrays for additional attributes which will be saved 
         # under ts.data
+        additional_keys = []
         if len(attrs) > 3:
-            for attribute_key in attrs[3:]:
+            for attribute_key in attrs:
+                # Skip the normal columns
+                if attribute_key == "id" or attribute_key in self._coordtype_column_names[self.lammps_coordinate_convention]: 
+                    continue
+                # Else this is an additional field
                 ts.data[attribute_key] = np.empty(self.n_atoms)
+                additional_keys.append(attribute_key)
 
         # Parse all the atoms
         for i in range(self.n_atoms):
@@ -623,12 +629,17 @@ class DumpReader(base.ReaderBase):
             ts.positions[i] = [fields[dim] for dim in coord_cols]
 
             # Add the capability to also collect other data
-            if len(attrs) > 3: # Then there is also more than just the positional data
-                for attribute_key in attrs[3:]:
+            if len(additional_keys) != 0: # Then there is also more than just the positional data
+                for attribute_key in additional_keys:
                     ts.data[attribute_key][i] = fields[attr_to_col_ix[attribute_key]]
 
         order = np.argsort(indices)
         ts.positions = ts.positions[order]
+
+        # Also need to sort the additional keys
+        for attribute_key in additional_keys:
+            ts.data[attribute_key] = ts.data[attribute_key][order]
+
         if (self.lammps_coordinate_convention.startswith("scaled")):
             # if coordinates are given in scaled format, undo that
             ts.positions = distances.transform_StoR(ts.positions,
